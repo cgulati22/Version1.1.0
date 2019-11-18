fb611a071bc3c40c69b5dcfe88d8f69b1a5d17e1
Chirag Gulati <c.gulati@vt.edu>
2019-11-18 15:36:32 -0500

    Double Linked List Complete

---
+++ Music Preference Visualization/src/Musis_Preference_Visualization/ListInterface.java
@@ -0,0 +1,11 @@
+package Musis_Preference_Visualization;
+
+public interface ListInterface<T> {
+
+
+    public T get(int index);
+    public boolean isEmpty();
+    public void clear();
+    public String toString();
+
+}
