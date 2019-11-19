2338c6d1e680898d62ced1719be67f495c72f323
Kavya Sundaram <smkavya@vt.edu>
2019-11-18 22:17:17 -0500

    final

--- Project5/src/prj5/Song.java
+++ Project5/src/prj5/Song.java
@@ -21,58 +21,25 @@ public class Song
     private CompareState compareState;
     private FileReader reader;
     
-    public Song(String artist, String songTitile)
+    public Song(String a, String st, int ry, String g)
     {
-        
+        artist = a;
+        songTitle = st;
+        releaseYear = ry;
+        genre = g;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public String getSongTitle() {
+        return songTitle;
+    }
+    public int getYear() {
+        return releaseYear;
+    }
+    public String getGenre() {
+        return genre;
     }
     
-    public void sortByArtist()
-    {
-        
-    }
     
-    public void sortBySongTitle()
-    {
-        
-    }
-    
-    public void sortByGenre()
-    {
-        
-    }
-    
-    public void sortByDate()
-    {
-        
-    }
-    
-    private void insertionSort()
-    {
-        
-    }
-    
-    public int getNumHobby()
-    {
-        
-    }
-    
-    public int getNumMajor()
-    {
-        
-    }
-    
-    public int getNumState()
-    {
-        
-    }
-    
-    public String getDate()
-    {
-        
-    }
-    
-    public int compareTo(Object obj)
-    {
-        
-    }
 }
